7655d691e6ae3e7db5c8d7eea94dee71e7a94c04
Filipe Dionisio <filipe.miguel.dionisio@gmail.com>
2022-01-13 13:06:39 +0000

    Joke App creation

---
+++ src/main/java/services/JokeServiceImpl.java
@@ -0,0 +1,17 @@
+package services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
